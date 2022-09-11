6781f1ae42b697dd6ea40db3ce1dfe5be7f84901
HedSut <bence02.info@gmail.com>
2022-09-11 14:24:14 +0200

    basic changes

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
+const path = require('path');
 const express = require('express'); //Import the express dependency
 const app = express();              //Instantiate an express app, the main work horse of this server
+const router = express.Router();
 const port = 5000;                  //Save the port number where your server will be listening
 
 //Idiomatic expression in express to route and respond to a client request
-app.get('/', (req, res) => {        //get requests to the root ("/") will route here
-    res.sendFile('index.html', {root: __dirname});      //server responds by sending the index.html file to the client's browser
+router.get('/', (req, res) => {        //get requests to the root ("/") will route here
+    res.sendFile(path.join(__dirname+'/index.html'));      //server responds by sending the index.html file to the client's browser
                                                         //the .sendFile method needs the absolute path to the file, see: https://expressjs.com/en/4x/api.html#res.sendFile 
 });
 
+router.get("/register", (req, res) => {
+    res.sendFile(path.join(__dirname+'/register/register.html'));
+});
+
+
+app.use(express.static("public"));
+app.use('/', router);
 app.listen(port, () => {            //server starts listening for any attempts from a client to connect at port: {port}
     console.log(`Now listening on port ${port}`); 
 });
