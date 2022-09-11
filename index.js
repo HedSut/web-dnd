4767c3b211fcc025504c27a1fdf350fb8384fd43
HedSut <bence02.info@gmail.com>
2022-09-11 14:43:54 +0200

    cleanup

--- index.js
+++ index.js
@@ -6,16 +6,17 @@ const port = 5000;                  //Save the port number where your server wil
 
 //Idiomatic expression in express to route and respond to a client request
 router.get('/', (req, res) => {        //get requests to the root ("/") will route here
-    res.sendFile(path.join(__dirname+'/index.html'));      //server responds by sending the index.html file to the client's browser
+    res.sendFile(path.join(__dirname+'/pages/index.html'));      //server responds by sending the index.html file to the client's browser
                                                         //the .sendFile method needs the absolute path to the file, see: https://expressjs.com/en/4x/api.html#res.sendFile 
 });
 
 router.get("/register", (req, res) => {
-    res.sendFile(path.join(__dirname+'/register/register.html'));
+    res.sendFile(path.join(__dirname+'/pages/register.php'));
 });
 
 
 app.use(express.static("public"));
+app.use(express.static("pages"));
 app.use('/', router);
 app.listen(port, () => {            //server starts listening for any attempts from a client to connect at port: {port}
     console.log(`Now listening on port ${port}`); 
